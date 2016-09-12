d6a883e1eb129f02b68fdcda7186bf6091c5b750
Yuhang <abeyuhang@gmail.com>
2016-09-12 19:15:26 +0800

    update

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 'use strict';
 
 const BaseModel = require('silence-js-base-model');
+const ModelField = BaseModel.ModelField;
 
 class SQLModel extends BaseModel {
   static get info() {
     if (!this.__info) {
       this.__info = {
+        fieldsTypeMap: {},
         fields: null,
         indices: null,
         table: null,
         primaryKey: null,
-        sql: null
+        sql: null,
+        autoUpdateTimestampField: null
       };
     }
     return this.__info;
@@ -31,19 +34,36 @@ class SQLModel extends BaseModel {
     return this.info.fields;
   }
   static set fields(fields) {
+    fields = fields.map(f => {
+      let field = new ModelField(f);
+      if (!field.name) {
+        this.logger.error(`Field must have 'name', please check fields of ${this.name}`);
+      } else if (!field.type) {
+        this.logger.error(`Field ${field.name} must have 'type', please check fields of ${this.name}`);
+      } else {
+        let result = this.db.initField(field);
+        if (result === -1) {
+          this.logger.error(`Unknown field type ${field.dbType || field.type}, please check fields of ${this.name}`);
+        } else if (result === -2) {
+          this.logger.error(`Unsupport defaultValue of field ${field.name}, please check fields of ${this.name}`);
+        } else if (result === -3) {
+          this.logger.error(`autoUpdate can only been applied to TIMESTAMP field with defaultValue 'now'`);
+        } else {
+          return field;
+        }
+      }
+      return null;
+    }).filter(f => !!f);
+
     for(let i = 0; i < fields.length; i++) {
-      let fd = fields[i];
-      if (!fd.name) {
-        throw new Error(`Field must have 'name'`);
-      } else if (!fd.type) {
-        throw new Error('Field must have \'type\'');
+      if (fields[i].autoUpdate) {
+        this.info.autoUpdateTimestampField = fields[i];
       }
-      let result = this.db.initField(fd);
-      if (result === -1) {
-        throw new Error(`Unknown field type ${fd.dbType || fd.type}`);
-      }
+      this.info.fieldsTypeMap[fields[i].name] = fields[i].type;
     }
+
     this.info.fields = fields;
+
   }
   static get table() {
     if (this.info.table === null) {
@@ -57,11 +77,8 @@ class SQLModel extends BaseModel {
   static get primaryKey() {
     if (this.info.primaryKey === null) {
       let fields = this.fields;
-      if (!Array.isArray(fields)) {
-        this.logger.error(`${this.name} model must have array type 'fields' property`);
-      }
       for(let i = 0; i < fields.length; i++) {
-        if (fields[i].primaryKey === true) {
+        if (fields[i].isPrimaryKey === true) {
           this.info.primaryKey = fields[i].name;
           return this.info.primaryKey;
         }
@@ -77,8 +94,8 @@ class SQLModel extends BaseModel {
     return this.db.exec(this.db.genCreateTableSQL(this));
   }
   static removeByPK(pk) {
-    return this.remove({
-      [this.primaryKey] : pk
+    return this.db.exec(`DELETE from ${this.table} WHERE \`${this.primaryKey}\`=?`, pk).then(result => {
+      return result.affectedRows;
     });
   }
   static remove(conditions) {
@@ -92,21 +109,50 @@ class SQLModel extends BaseModel {
       return result.affectedRows;
     });
   }
+
+  static updateByPK(pk, fields) {
+    return this.update(pk, fields);
+  }
+
   static update(conditions, fields) {
     let queryFields = [], queryParams = [];
     for(let k in fields) {
       if (k === this.primaryKey) {
         continue;
       }
+      let type = this.info.fieldsTypeMap[k];
+      if (!type) {
+        continue;
+      }
+      let v = fields[k];
       queryFields.push(`\`${k}\`` + '=?');
-      queryParams.push(fields[k]);
+      queryParams.push(type === 'boolean' ? (v ? 1 : 0) : v);
     }
-    let conditionFields = [];
-    for(let k in conditions) {
-      conditionFields.push(`\`${k}\`` + '=?');
-      queryParams.push(conditions[k]);
+    let af = this.info.autoUpdateTimestampField;
+    if (af && !fields.hasOwnProperty(af.name)) {
+      queryFields.push(`\`${af.name}\`=?`);
+      queryParams.push(af.defaultValue);
     }
-    let queryString = `UPDATE \`${this.table}\` SET  ${queryFields.join(', ')} WHERE ${conditionFields.join(' AND ')}`;
+
+    let queryString;
+
+    if (typeof conditions === 'object') {
+      let conditionFields = [];
+      for(let k in conditions) {
+        let type = this.info.fieldsTypeMap[k];
+        if (!type) {
+          continue;
+        }
+        let v = conditions[k];
+        conditionFields.push(`\`${k}\`=?`);
+        queryParams.push(type === 'boolean' ? (v ? 1 : 0) : v);
+      }
+      queryString = `UPDATE \`${this.table}\` SET  ${queryFields.join(', ')} WHERE ${conditionFields.join(' AND ')}`;
+    } else {
+      queryString = `UPDATE \`${this.table}\` SET  ${queryFields.join(', ')} WHERE ${this.primaryKey}=?`;
+      queryParams.push(conditions);
+    }
+
     return this.db.exec(queryString, queryParams).then(result => {
       return result.affectedRows > 0;
     });
@@ -117,14 +163,25 @@ class SQLModel extends BaseModel {
     let fields = typeof options.fields === 'string' ? options.fields : (Array.isArray(options.fields) ? options.fields.join(',') : '*');
     let conditionFields = [], conditionParams = [];
     for(let k in conditions) {
-      conditionFields.push(`\`${k}\`` + '=?');
-      conditionParams.push(conditions[k]);
+      let type = this.info.fieldsTypeMap[k];
+      if (!type) {
+        continue;
+      }
+      let v = conditions[k];
+      conditionFields.push(`\`${k}\`=?`);
+      conditionParams.push(type === 'boolean' ? (v ? 1 : 0) : v);
     }
     let conditionString = conditionFields.length > 0 ? `WHERE ${conditionFields.join(' AND ')}` : '';
     let limitString = options.limit ? ("LIMIT " + (options.offset ? options.offset + ', ' : '') + options.limit) : '';
     let orderString = options.orderBy ? "ORDER BY " + (_.isArray(options.orderBy) ? options.orderBy.join(',') : options.orderBy) : '';
     let queryString = `SELECT ${fields} from ${this.table} ${conditionString} ${orderString} ${limitString};`;
-    return this.db.query(queryString, conditionParams);
+    return this.db.query(queryString, conditionParams).then(rows => {
+      return rows.map(row => {
+        let m = new this(false);
+        m.assign(row);
+        return m;
+      });
+    });
   }
   static one(primaryKeyOrConditions = {}, options) {
     let conditions = typeof primaryKeyOrConditions === 'object'
@@ -161,32 +218,32 @@ class SQLModel extends BaseModel {
       return result[0].N;
     });
   }
-  _saveOrUpdate(updateConditions, validate) {
-    if (validate && !this.validate(updateConditions !== null)) {
+  _saveOrUpdate(updatePK, validate) {
+    if (validate && !this.validate(updatePK !== null)) {
       return Promise.resolve(false);
     }
     let fields = this.constructor.fields;
     let queryFields = [], queryParams = [];
     for(let i = 0; i < fields.length; i++) {
-      let f = fields[i].name;
-      if (f === this.constructor.primaryKey && fields[i].autoIncrement) {
-        continue;
-      }
-      if (this[f] !== undefined) {
-        queryFields.push(`\`${f}\`${updateConditions ? '=?' : ''}`);
-        queryParams.push(this[f]);
+      let fn = fields[i].name;
+      let v = this[fn];
+      if (fn !== this.constructor.primaryKey &&  v !== undefined) {
+        queryFields.push(`\`${fn}\`${updatePK ? '=?' : ''}`);
+        queryParams.push(typeof v === 'boolean' ? (v ? 1 : 0) : v);
       }
     }
     let queryString;
-    if (updateConditions) {
-      let conditionFields = [];
-      for(let k in updateConditions) {
-        conditionFields.push(`\`${k}\`` + '=?');
-        queryParams.push(updateConditions[k]);
+    if (updatePK) {
+      let af = this.constructor.info.autoUpdateTimestampField;
+      if (af && this[af.name] === undefined) {
+        this[af.name] = af.defaultValue;
+        queryFields.push(`\`${af.name}\`=?`);
+        queryParams.push(this[af.name]);
       }
-      queryString = `UPDATE ${this.constructor.table} SET  ${queryFields.join(', ')} WHERE ${conditionFields.join(' AND ')}`;
+      queryParams.push(this[updatePK]);
+      queryString = `UPDATE ${this.constructor.table} SET ${queryFields.join(', ')} WHERE \`${updatePK}\`=?`;
     } else {
-      queryString = `INSERT INTO ${this.constructor.table} (${queryFields.join(',')}) VALUES (${queryFields.map(()=>'?').join(',')})`;
+      queryString = `INSERT INTO ${this.constructor.table} (${queryFields.join(', ')}) VALUES (${queryFields.map(()=>'?').join(', ')})`;
     }
     return this.constructor.db.exec(queryString, queryParams)
   }
@@ -196,22 +253,23 @@ class SQLModel extends BaseModel {
         return false;
       }
       let pk = this.constructor.primaryKey;
-      this[pk] = result.insertId;
+      pk && (this[pk] = result.insertId);
       return true;
     });
   }
   update(validate = true) {
     let pk = this.constructor.primaryKey;
-    return this._saveOrUpdate({
-      [pk] : this[pk]
-    }, validate).then(result => {
+    if (!pk || typeof this[pk] === 'undefined' || this[pk] === null) {
+      return Promise.reject(`update can only been applied to model with primaryKey, please check model: ${this.constructor.name}`);
+    }
+    return this._saveOrUpdate(pk, validate).then(result => {
       return result.affectedRows > 0;
     });
   }
   remove() {
     let pk = this.constructor.primaryKey;
-    if (!pk || !this[pk]) {
-      return Promise.resolve(false);
+    if (!pk || typeof this[pk] === 'undefined' || this[pk] === null) {
+      return Promise.reject(`remove can only been applied to model with primaryKey, please check model: ${this.constructor.name}`);
     }
     return this.constructor.remove({
       [pk]: this[pk]
