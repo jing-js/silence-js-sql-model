4085deb76e392d0dddc32b0a218efa71fb8954aa
Yuhang <abeyuhang@gmail.com>
2016-09-20 13:29:29 +0800

    update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const BaseSQLModel = require('./BaseSQLModel');
 const { BaseModel } = require('silence-js-base-model');
 
 const SQLModel = {
+  BaseSQLModel,
   __init(db, logger) {
     if (BaseSQLModel.__db) {
       throw new Error('BaseSQLModel.__db already exists. __init can be called only once.');
